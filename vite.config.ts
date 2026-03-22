f74fc488749f001785e1124e437c6065afb7584b
ruslan.krasiuk <rus.krasuk@gmail.com>
2026-03-22 17:07:44 +0200

    build: fixes

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     return {
+        base: process.env.VITE_BASE ?? '/',
         plugins: [
             devtools(),
             tanstackRouter({
